ac549cd386c599717c4945f8b702dfb92165ba4a
RenzoRebatta <i201723576@cibertec.edu.pe>
2021-09-26 22:48:40 -0500

    Actividad05

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddDisponibilidadComponent } from './componenets/add-disponibilidad/add-disponibilidad.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddDisponibilidadComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componenets/add-disponibilidad/add-disponibilidad.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { Ciclo } from 'src/app/models/ciclo.model';
+import { Disponibilidad } from 'src/app/models/disponibilidad.model';
+import { Usuario } from 'src/app/models/usuario.model';
+import { CicloService } from 'src/app/services/ciclo.service';
+import { DisponibilidadService } from 'src/app/services/disponibilidad.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
+
+@Component({
+  selector: 'app-add-disponibilidad',
+  templateUrl: './add-disponibilidad.component.html',
+  styleUrls: ['./add-disponibilidad.component.css']
+})
+export class AddDisponibilidadComponent implements OnInit {
+
+  lstCiclo: Ciclo[] = [];
+
+  lstUsuario: Usuario[] = [];
+
+  disponibilidad: Disponibilidad = {
+    horaInicio: 0,
+    horaFin: 0,
+    dia: "",
+    ciclo: {
+      idCiclo: 0,
+    },
+    usuario: {
+      idUsuario: 0,
+    }
+  }
+
+  constructor(private cicloService: CicloService, private usuarioService: UsuarioService, private disponibilidadService: DisponibilidadService) {
+    this.cicloService.listaCiclo().subscribe(
+      ciclo => {
+        this.lstCiclo = ciclo;
+      }
+    );
+    this.usuarioService.listaUsuario().subscribe(
+      usuario =>{
+        this.lstUsuario = usuario;
+      }
+    )
+  }
+  
+
+  registra() {
+    console.log(">>> registra() ");
+    console.log(this.disponibilidad);
+    this.disponibilidadService.create(this.disponibilidad).subscribe(
+      response => {
+        console.log(response.mensaje);
+        alert(response.mensaje)
+      },
+      error => {
+        console.log(error);
+      },
+    );
+  }
+
+
+
+  ngOnInit(): void {
+  }
+
+}
